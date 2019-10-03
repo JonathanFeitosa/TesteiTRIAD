abce97e717c2bd6d783b1a0b3d707d81bef3f6be
Jonathan Feitosa <jfsjonathanfeitosa@gmail.com>
2019-10-02 23:55:47 -0400

    Add NavBar,Layouts,Fragments

---
+++ app/src/main/java/br/org/itriad/testeitriad/ListRepositoryActivity.java
@@ -0,0 +1,32 @@
+package br.org.itriad.testeitriad;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
+
+public class ListRepositoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.host_listrepository);
+
+        // Toolbar fragments integrate
+            NavigationUI.setupActionBarWithNavController(
+                this,
+                Navigation.findNavController(this, R.id.navHostFragment)
+            );
+
+    }
+
+   // Button Back toolbar
+    @Override
+     public boolean onSupportNavigateUp() {
+        return Navigation.findNavController(this, R.id.navHostFragment).navigateUp();
+    }
+}
